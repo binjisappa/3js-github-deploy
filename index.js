490effeb80f8dc9ebc5b7f5f47d2723114db11fd
Lyndon Clark <ldbbclark@gmail.com>
2021-08-30 09:34:02 +1200

    Update index.js
    changed camera to 14

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 
-camera.position.set(0, 0, 4);
+camera.position.set(0, 0, 14);
 scene.add(camera);
 
 const renderer = new THREE.WebGL1Renderer({
